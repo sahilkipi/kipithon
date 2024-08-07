fef5c31b86c04709c0adc2df14dc7cf1fe13d2de
sharada <sharada.s.mallya@kipi.bi>
2024-08-07 16:04:52 +0530

    Reporting query commit1

--- REPORTING_QUERY.sql
+++ REPORTING_QUERY.sql
@@ -13,8 +13,6 @@ SELECT
 FROM KIPITHON_DB.LANDING.SCHEMA_MEMORY_DETAILS_STG;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
---YET TO EXECUTE
-
 
 CREATE OR REPLACE VIEW KIPITHON_DB.REPORTING.DB_QUERY_TABLE_TYPE --- SAMPLE_DB.REPORTING.ACCESS_COUNT_TBL 
 AS
@@ -22,10 +20,10 @@ SELECT
     AC.DB_NAME AS DB_NAME,
     AC.TABLE_NAME AS TABLE_NAME, 
     AC.EXEC_COUNT AS EXEC_COUNT, 
-    RL.TABLE_TYPE AS TABLE_TYPE
+    RL.OBJECT_TYPE AS TABLE_TYPE
 FROM KIPITHON_DB.LANDING.QUERY_HISTORY_RESULTS_EXTRACT AC
 JOIN KIPITHON_DB.REPORTING.SCHEMA_MEMORY_DETAILS RL
-ON AC.TABLE_NAME=RL.TABLE_NAME AND AC.DB_NAME=RL.TABLE_SCHEMA;
+ON AC.TABLE_NAME=RL.OBJECT_NAME AND AC.DB_NAME=RL.OBJECT_SCHEMA;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -47,7 +45,21 @@ FROM KIPITHON_DB.LANDING.SCHEMA_MEMORY_DETAILS_STG
 GROUP BY TABLE_SCHEMA;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW KIPITHON_DB.REPORTING.DB_HOST_SUMMARY
+AS
+SELECT 
+	STATEMENTS,
+	STATEMENT_LATENCY,
+	TABLE_SCANS,
+	TOTAL_CONNECTIONS,
+	CURRENT_MEMORY,
+	TOTAL_MEMORY_ALLOCATED,
+	INGEST_DATE
+FROM KIPITHON_DB.LANDING.DB_HOST_SUMMARY_STG
+;
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW KIPITHON_DB.REPORTING.DB_HOST_SUMMARY
 CREATE OR REPLACE TABLE KIPITHON_DB.REPORTING.CREDENTIALS ( --- SAHIL_DB.credentials table
     USERNAME VARCHAR NOT NULL,
     NAME VARCHAR,
